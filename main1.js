2bf1aa25d113d2f1838d0a9669b4fd551d8fcfca
Jarvis555 <jarvisjackson591@gmail.com>
2024-11-22 18:23:33 -0600

    first commit

---
+++ main1.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let btn = document.getElementById("btn");
+  let txt = document.getElementById("txt");
+  let apikey = "51af69a3";
+  btn.addEventListener("click", function () {
+    fetch("http://www.omdbapi.com/?apikey=" + apikey + "&t=" + txt.value)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myJson) {
+        console.log(myJson);
+        let movieInfo = document.getElementById("movieInfo");
+        let movie = document.getElementById("movie");
+        let title = myJson.Title;
+        movieInfo.innerHTML = `<strong>Title:</strong> ${title}`;
+        movieInfo.innerHTML += "<br>";
+        movieInfo.innerHTML += `<strong>Plot:</strong> ${myJson.Plot}`;
+        myJson.Actors.split(",").forEach(function (actor) {
+          movieInfo.innerHTML += actor + " | ";
+        });
+        movieInfo.innerHTML += "<br>";
+        myJson.Ratings.forEach(function (rating) {
+          movieInfo.innerHTML += rating.Source + ":" + rating.Value + "<br>";
+        });
+        let link = document.createElement("a");
+        link.href = "https://www.imdb.com/title/" + myJson.imdbID;
+        let img = document.createElement("img");
+        img.src = myJson.Poster;
+        img.style.width = "25vw";
+        link.appendChild(img);
+        movie.appendChild(link);
+      });
+  });
+});
